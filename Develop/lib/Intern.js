52032f2039c0de10a61644092f2be75f0afe3e5c
Jason McDonald <jasonmcd65@gmail.com>
2020-12-31 13:11:31 -0600

    Created Intern, tests good

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name, id, email, school, role = 'Intern'){
+        super(name, id, email)
+        this.role = role
+        this.school = school
+    }
+
+    // Getters and Setters
+    getSchool(){
+        return this.school
+    }
+    
+    setSchool(newSchool){
+        this.school = newSchool
+    }
+}
+
+module.exports = Intern
