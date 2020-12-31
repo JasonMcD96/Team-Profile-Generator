a071ca7ebe92cf2d68c56304fd3518e4887739ff
Jason McDonald <jasonmcd65@gmail.com>
2020-12-31 13:10:58 -0600

    Created parent Employee, tests good

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,41 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email, role = 'Employee'){
+        this.name = name
+        this.id = id
+        this.email = email
+        this.role = role
+    }
+
+    // Getters and Setters
+    getName(){
+        return this.name
+    }
+
+    setName(newName){
+        this.name = newName
+    }
+
+    setId(newID){
+        this.id = newID
+    }
+
+    getId(){
+        return this.id
+    }
+
+    serEmail(newEmail){
+        this.email = newEmail
+    }
+
+    getEmail(){
+        return this.email
+    }
+
+    getRole(){
+        return this.role
+    }
+}
+
+module.exports = Employee
